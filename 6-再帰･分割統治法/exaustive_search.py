72de5bb42a32229d07ad217dc8495f8ffdf95308
永木郁也 <ikuya@198m2.local>
2023-04-09 12:33:57 +0900

    learn recursive function

--- 6-再帰･分割統治法/exaustive_search.py
+++ 6-再帰･分割統治法/exaustive_search.py
@@ -1,1 +1,30 @@
+# https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/5/ALDS1_5_A
 
+def can_make(components, idx, target):
+  if target == 0:
+    return True
+  if idx >= len(components):
+    return False
+  return can_make(components, idx+1, target) or can_make(components, idx+1, target - components[idx])
+
+
+#main
+qty = input()
+components = list(map(int, input().split(' ')))
+qty = input()
+targets = list(map(int, input().split(' ')))
+for t in targets:
+  print("yes") if can_make(components, 0, t) else print("no")
+
+"""input
+5
+1 5 7 10 21
+4
+2 4 17 8
+"""
+"""output
+no
+no
+yes
+yes
+"""
